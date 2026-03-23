c1e4a9f7ee8173fc4f64aa52b6ce98e239045f5f
jose260102uib <jose.luque2@estudiant.uib.cat>
2026-03-23 18:11:15 +0100

    Configuración pipelines entornos.

---
+++ backend/src/test/java/com/napoleon/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.napoleon;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
