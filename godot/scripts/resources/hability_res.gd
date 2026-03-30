fbedd36acedf94b2e6f694949cf8d4d358e9030b
XanaDevelops <danielgv.space@gmail.com>
2026-03-30 18:43:23 +0200

    modifica la habilidad para que tenga condicionalmente un estado alterado

--- godot/scripts/resources/hability_res.gd
+++ godot/scripts/resources/hability_res.gd
@@ -21,6 +21,11 @@ enum HAB_DEST {SELF,
 	SINGLE_ANY, MULTIPLE_ANY, 
 	EVERYONE}
 
+enum CONDITION {
+	LT, GT, EQ, LE, GE, NE,
+	NA ## Not Aplicable
+}
+
 ## Objetivo de la habilidad (enum HAB_DEST)
 @export var objective := HAB_DEST.SINGLE_ENEMY
 ## Valor de la habilidad
@@ -39,3 +44,11 @@ enum HAB_DEST {SELF,
 @export var isPassive := false
 ## Estados alterados aplicados (Array[[AlterStateRes]])
 @export var alter_states: Array[AlterStateRes]
+## tipo de condicion
+@export var condition:= CONDITION.NA
+## estadistica a comparar
+@export var condition_stat : StatData = null
+## valor a comparar
+@export var condition_value := 0.0
+## estado alterado por si se cumple la condicion
+@export var alter_state_conditio : AlterStateRes
