5bba461bc5dcb57ebcd791d189d4659729f7aabf
Ti-trit <khaoulaberrada395@gmail.com>
2026-04-05 13:20:29 +0200

    selection and highlight layers

--- godot/scenes/ingame/map_visualizer.gd
+++ godot/scenes/ingame/map_visualizer.gd
@@ -6,57 +6,104 @@ var map:MapGame
 
 @export var tile_map_layer_texture: TileMapLayer
 @export var tile_map_layer_units: TileMapLayer
+@export var tile_map_layer_selection: TileMapLayer
+@export var tile_map_layer_highlight: TileMapLayer
 
 var tileset: TileSet
 var texture_to_source_id: Dictionary = {}
+enum HighlightType { MOVEMENT, ATTACK, SELECTED, SKILL }
 
+var highlight_source_ids: Dictionary[HighlightType, int] = {}
+
+const HIGHLIGHT_TEXTURES: Dictionary = {
+    HighlightType.MOVEMENT: "res://assets/tiles/highlights/hl_move.png",
+    HighlightType.ATTACK:   "res://assets/tiles/highlights/hl_attack.png",
+    HighlightType.SELECTED: "res://assets/tiles/highlights/hl_selected.png",
+}
+const TILE_SIZE = 64
+func _setup_highlight_tiles() -> void:
+    for type in HIGHLIGHT_TEXTURES.keys():
+        var tex := load(HIGHLIGHT_TEXTURES[type]) as Texture2D
+        highlight_source_ids[type] = add_texture_to_tileset(tex)
+        
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#map = GameManagerNode.get_map()
-	map = TestMapGame.create_test_map()  
-	tileset= TileSet.new()
-	tileset.tile_shape = TileSet.TILE_SHAPE_HEXAGON
-	tileset.tile_layout = TileSet.TILE_LAYOUT_STACKED        
-	tileset.tile_offset_axis = TileSet.TILE_OFFSET_AXIS_HORIZONTAL  
-	tileset.tile_size = Vector2i(128, 128)
-	tile_map_layer_texture.position = Vector2(-10, -10) 
-	tile_map_layer_texture.tile_set= tileset
-	tile_map_layer_units.tile_set = tileset
-	setup_map()
+    #map = GameManagerNode.get_map()
+    map = TestMapGame.create_test_map()  
+    tileset = _setup_tileset()
+    for tml in [tile_map_layer_texture, tile_map_layer_units,
+                tile_map_layer_selection, tile_map_layer_highlight]:
+        tml.tile_set = tileset
+    _setup_highlight_tiles()
+    _setup_map()
+  
+func _setup_tileset() -> TileSet:
+    var tileset = TileSet.new()
+    tileset.tile_shape = TileSet.TILE_SHAPE_HEXAGON
+    tileset.tile_layout = TileSet.TILE_LAYOUT_STACKED        
+    tileset.tile_offset_axis = TileSet.TILE_OFFSET_AXIS_HORIZONTAL  
+    tileset.tile_size = Vector2i(TILE_SIZE, TILE_SIZE)
+    return tileset
+    
 
-func setup_map():
-	#read tiles 
-	for x in range(map._mapRes.tamX):
-		for y in range(map._mapRes.tamY):
-			draw_tile(x, y, map.get_tile_at(x, y))
-			
+func _setup_map():
+    for x in range(map._mapRes.tamX):
+        for y in range(map._mapRes.tamY):
+            draw_tile(x, y, map.get_tile_at(x, y))
+            
 
 func draw_tile(i: int, y:int, tile:TileGame) -> void:
-		var coords = Vector2i(i, y)
-		
-		var tile_source_id = add_texture_to_tileset(tile.get_texture2D())
-				
-		tile_map_layer_texture.set_cell(coords, tile_source_id, Vector2i.ZERO)
-		
-		if tile.has_unit():
-			var unit_source_id = add_texture_to_tileset(tile.get_unit().get_texture2D())
-			tile_map_layer_units.set_cell(coords, unit_source_id, Vector2i.ZERO)
-		
+    
+        var coords = Vector2i(i, y)
+        var tile_source_id = add_texture_to_tileset(tile.get_texture2D())
+                
+        tile_map_layer_texture.set_cell(coords, tile_source_id, Vector2i.ZERO)
+        
+        if tile.has_unit():
+            var unit_source_id = add_texture_to_tileset(tile.get_unit().get_texture2D())
+            tile_map_layer_units.set_cell(coords, unit_source_id, Vector2i.ZERO)
+
+func _scale_texture(texture: Texture2D) -> ImageTexture:
+    var img := texture.get_image()
+    img.resize(TILE_SIZE, TILE_SIZE, Image.INTERPOLATE_NEAREST)
+    return ImageTexture.create_from_image(img)
+    
 func add_texture_to_tileset(texture: Texture2D) -> int:
-		for tex_id in texture_to_source_id.keys():
-			if texture_to_source_id[tex_id]== texture:
-				return tex_id
-			
-		var source_id = texture_to_source_id.size()
-		var tile_set_source = TileSetAtlasSource.new()
-		tile_set_source.texture= texture
-		tile_set_source.texture_region_size= Vector2i(texture.get_width(), texture.get_height())
-		tile_set_source.create_tile(Vector2i.ZERO)
-		tileset.add_source(tile_set_source, source_id)
-		texture_to_source_id[source_id]= texture
-		return source_id	
-	
-	
+        for tex_id in texture_to_source_id.keys():
+            if texture_to_source_id[tex_id]== texture:
+                return tex_id
+        
+        var source_id = texture_to_source_id.size()
+        var tile_set_source = TileSetAtlasSource.new()
+        var texture_resized = _scale_texture(texture)
+        tile_set_source.texture= texture_resized
+        tile_set_source.texture_region_size= Vector2i(texture_resized.get_width(), texture_resized.get_height())
+        tile_set_source.create_tile(Vector2i.ZERO)
+        
+        tileset.add_source(tile_set_source, source_id)
+        texture_to_source_id[source_id]= texture_resized
+        return source_id
+    
+    
+func plot_unit_moved(src: Vector2i, target:Vector2i) -> void:
+        var source_id = tile_map_layer_units.get_cell_source_id(src)
+        tile_map_layer_units.set_cell(target, source_id, Vector2i.ZERO)
+        tile_map_layer_units.erase_cell(src)
+        
+func refresh_unit_died(coords: Vector2i):
+    tile_map_layer_units.erase_cell(coords)
+
+func highlight_selected_cell(pos: Vector2i) -> void:
+    tile_map_layer_selection.clear()
+    tile_map_layer_selection.set_cell(pos, highlight_source_ids[HighlightType.SELECTED], Vector2i.ZERO)
+
+func plot_mov_range(positions: Array[Vector2i]):
+    tile_map_layer_highlight.clear()
+    for pos in positions: 
+        tile_map_layer_highlight.set_cell(pos, highlight_source_ids[HighlightType.MOVEMENT], Vector2i.ZERO)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+    #recibe señales
+    #update stuff
+    
+    pass
--- godot/test/scenes/testMapGame.gd
+++ godot/test/scenes/testMapGame.gd
@@ -1,77 +1,77 @@
 class_name TestMapGame
 extends Node
 static func create_test_map() -> MapGame:
-    var map_res = MapRes.new()
-    map_res.name = "Test Map"
-    map_res.desc = "Mapa de prueba"
-    map_res.tamX = 5
-    map_res.tamY = 5
-    
-    for y in range(map_res.tamX):
-        var row: Array = []
-        for x in range(map_res.tamY):
-            var tile_res = TileRes.new()
-            tile_res.height = 0
-            
-            var tile_type = TileTypeRes.new()
-            tile_type.name = "GrassLand"
-            tile_type.desc = "Terrain de prueba"
-            tile_type.texture = get_random_tile_texture("res://assets/tiles/EverHex-Forest Lite/Sky/blue")
-            tile_res.type = tile_type
-            row.append(tile_res)
-        
-        map_res.mapData.push_back(row)
-    
-    var map = MapGame.new(map_res)
-    
-    for y in range(map_res.tamX):
-        for x in range(map_res.tamY):
-            if randi() % 2 == 0:
-                var card_res = CardRes.new()
-                card_res.name = "Warrior"
-                card_res.hp = 10
-                card_res.img = get_unit_texture("res://assets/tiles/Legacy-Fantasy - High Forest 2.0/Legacy-Fantasy - High Forest 2.3/Character/Idle/Idle-Sheet.png")
-                
-                var unit = UnitGame.new(card_res)
-                map.get_tile_at(x, y).set_unit(unit)
-    
-    return map
+	var map_res = MapRes.new()
+	map_res.name = "Test Map"
+	map_res.desc = "Mapa de prueba"
+	map_res.tamX = 5
+	map_res.tamY = 5
+	
+	for y in range(map_res.tamX):
+		var row: Array = []
+		for x in range(map_res.tamY):
+			var tile_res = TileRes.new()
+			tile_res.height = 0
+			
+			var tile_type = TileTypeRes.new()
+			tile_type.name = "GrassLand"
+			tile_type.desc = "Terrain de prueba"
+			tile_type.texture = get_random_tile_texture("res://assets/tiles/EverHex-Forest Lite/Sky/blue")
+			tile_res.type = tile_type
+			row.append(tile_res)
+		
+		map_res.mapData.push_back(row)
+	
+	var map = MapGame.new(map_res)
+	
+	for y in range(map_res.tamX):
+		for x in range(map_res.tamY):
+			if randi() % 2 == 0:
+				var card_res = CardRes.new()
+				card_res.name = "Warrior"
+				card_res.hp = 10
+				card_res.img = get_unit_texture("res://assets/tiles/Legacy-Fantasy - High Forest 2.0/Legacy-Fantasy - High Forest 2.3/Character/Idle/Idle-Sheet.png")
+				
+				var unit = UnitGame.new(card_res)
+				map.get_tile_at(x, y).set_unit(unit)
+	
+	return map
 
 static func get_unit_texture(sheet_path: String) -> Texture2D:
-    var texture = load(sheet_path) as Texture2D
-    if texture == null:
-        push_error("No se puede cargar: " + sheet_path)
-        return null
-    
-    var sprite_width = 64   # 256 / 4 sprites
-    var sprite_height = 80
-    var index = randi() % 4
-    
-    var atlas = AtlasTexture.new()
-    atlas.atlas = texture
-    atlas.region = Rect2(index * sprite_width, 0, sprite_width, sprite_height)
-    return atlas
-    
+	var texture = load(sheet_path) as Texture2D
+	if texture == null:
+		push_error("No se puede cargar: " + sheet_path)
+		return null
+	
+	var sprite_width = 64   # 256 / 4 sprites
+	var sprite_height = 80
+	var index = randi() % 4
+	
+	var atlas = AtlasTexture.new()
+	atlas.atlas = texture
+	atlas.region = Rect2(index * sprite_width, 0, sprite_width, sprite_height)
+	return atlas
+	
 
 
 static func get_random_tile_texture(folder_path: String) -> Texture2D:
-    var dir = DirAccess.open(folder_path)
-    if dir == null:
-        push_error("No se puede abrir la carpeta: " + folder_path)
-        return null
-    
-    var files: Array = []
-    dir.list_dir_begin()
-    var file_name = dir.get_next()
-    while file_name != "":
-        if file_name.ends_with(".png") or file_name.ends_with(".jpg"):
-            files.append(folder_path + "/" + file_name)
-        file_name = dir.get_next()
-    dir.list_dir_end()
-    
-    if files.is_empty():
-        push_error("No hay texturas en: " + folder_path)
-        return null
-    
-    var random_file = files[randi() % files.size()]
-    return load(random_file)
+	var dir = DirAccess.open(folder_path)
+	if dir == null:
+		push_error("No se puede abrir la carpeta: " + folder_path)
+		return null
+	
+	var files: Array = []
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if file_name.ends_with(".png") or file_name.ends_with(".jpg"):
+			files.append(folder_path + "/" + file_name)
+		file_name = dir.get_next()
+	dir.list_dir_end()
+	
+	if files.is_empty():
+		push_error("No hay texturas en: " + folder_path)
+		return null
+	
+	var random_file = files[randi() % files.size()]
+	return load(random_file)
