afa211f60b81dbbe5802cb4fb3a21464d186470c
Pere55 <truficonosmanut@gmail.com>
2026-04-09 13:53:03 +0200

    creació de la pantalla de selecció de mapas i conexió de les pantallas principals

--- godot/scripts/managers/user_manager.gd
+++ godot/scripts/managers/user_manager.gd
@@ -1,11 +1,34 @@
 extends Node
 
+@export var mapas_de_prueba: Array[MapRes] = []
+var jugador_1: UserRes
+var jugador_2: UserRes
+
+
 var usuario_actual: UserRes
 
+func _ready() -> void:
+	
+	jugador_1 = TestUserGenerator.new().generar_usuario_completo()
+	jugador_1.name = "Jugador 1" 
+	
+	jugador_2 = TestUserGenerator.new().generar_usuario_completo()
+	jugador_2.name = "Jugador 2"
+	
+	
+	usuario_actual = jugador_1
 
-
-func establecer_usuario_actual(nuevo_usuario: UserRes) -> void:
-	usuario_actual = nuevo_usuario
+func set_jugador_activo(es_jugador_uno: bool) -> void:
+	if es_jugador_uno:
+		usuario_actual = jugador_1
+	else:
+		usuario_actual = jugador_2
+	print("El menú ahora está editando a: " + usuario_actual.name)
+	
+func obtener_mapas() -> Array[MapRes]:
+	if usuario_actual == null or usuario_actual.availableMaps.is_empty():
+		return mapas_de_prueba
+	return usuario_actual.availableMaps
 
 func obtener_ejercitos() -> Array:
 	if usuario_actual == null or usuario_actual.userArmys == null:
@@ -15,8 +38,7 @@ func obtener_ejercitos() -> Array:
 func existe_ejercito(nombre_a_comprobar: String) -> bool:
 	var ejercitos = obtener_ejercitos()
 	for ejercito in ejercitos:
-		if ejercito.nom == nombre_a_comprobar:
-			return true
+		if ejercito.nom == nombre_a_comprobar: return true
 	return false
 
 func establecer_ejercito_activo(nombre_ejercito_a_activar: String) -> void:
@@ -25,27 +47,21 @@ func establecer_ejercito_activo(nombre_ejercito_a_activar: String) -> void:
 		ejercito.isActive = (ejercito.nom == nombre_ejercito_a_activar)
 
 func guardar_ejercito(ejercito_a_guardar: ArmyRes, nombre_anterior: String) -> void:
+	if usuario_actual == null: return
 	var ejercitos = usuario_actual.userArmys
 	var encontrado = false
-	
 	for i in range(ejercitos.size()):
 		if ejercitos[i].nom == nombre_anterior:
 			ejercitos[i] = ejercito_a_guardar.clonar()
 			encontrado = true
 			break
-			
 	if not encontrado:
 		ejercitos.append(ejercito_a_guardar.clonar())
 
 func eliminar_ejercito(nombre_ejercito: String) -> void:
+	if usuario_actual == null: return
 	var ejercitos = usuario_actual.userArmys
-	var era_activo = false
-	
 	for i in range(ejercitos.size() - 1, -1, -1):
 		if ejercitos[i].nom == nombre_ejercito:
-			era_activo = ejercitos[i].isActive 
 			ejercitos.remove_at(i)
 			break
-			
-	if era_activo and ejercitos.size() > 0:
-		ejercitos[0].isActive = true
