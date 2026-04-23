58cd1e8bd90c44809a16e72b137825179f652fdd
Daniel GV <danielgv.space@gmail.com>
2026-04-23 17:58:04 +0200

    modificado calculo plano de distancia

--- godot/scripts/game/map_game.gd
+++ godot/scripts/game/map_game.gd
@@ -59,10 +59,21 @@ func move_unit(start: Vector2i, end: Vector2i) -> void:
 	get_tile_at(start).set_unit(null)
 	get_tile_at(end).set_unit(unit)
 	
+## Devuelve las posiciones con tropas dentro del rango
+func get_units_range(pos: Vector2i, range: int) -> Array[Vector2i]:
+	var ret_pos : Array[Vector2i] = []
+	var tile := get_tile_at(pos)
+	
+	if !tile.has_unit():
+		return ret_pos
+		
+	
+	return ret_pos
+	
+	
 ## Devuelve las posiciones de casillas accesibles para la unidad en 'pos'
 ## devuelve [] si no hay unidad
 ## En godot/scripts/game/map_game.gd
-
 func get_accesible_moves(pos: Vector2i) -> Array[Vector2i]:
 	var ret_pos : Array[Vector2i] = []
 	var tile := get_tile_at(pos)
@@ -74,15 +85,16 @@ func get_accesible_moves(pos: Vector2i) -> Array[Vector2i]:
 	var distances := _calculate_distances(pos, unit)
 	
 	for key in distances:
-		
 		if distances[key] <= unit.get_speed() and key != pos:
-			
 			if not get_tile_at(key).has_unit():
 				ret_pos.append(key)
 		
 	return ret_pos
 	
+## Good ol' Dijkstra
 func _calculate_distances(pos: Vector2i, unit: UnitGame) -> Dictionary[Vector2i, int]:
+	const MAX_INT := 9223372036854775807
+	
 	var distances : Dictionary[Vector2i, int] = {pos: 0}
 	# No existen genericos, no tipas la lambda
 	var queue : PriorityQueue = PriorityQueue.new(false, [], func(x): return x.values()[0])
@@ -94,14 +106,18 @@ func _calculate_distances(pos: Vector2i, unit: UnitGame) -> Dictionary[Vector2i,
 		var _pos : Vector2i = elem.keys()[0]
 		var _dist : int= elem[_pos]
 		
-		if _dist > distances.get(_pos, 9223372036854775807):
+		if _dist > distances.get(_pos, MAX_INT):
 			continue
 		
 		for neight in get_neightbours(_pos):
-			var _cost := get_tile_at(neight).get_cost(unit)
-			# penalización altura
-			_cost += get_tile_at(_pos).get_height_penalty(get_tile_at(neight))
-			if distances.get(_pos) + _cost < distances.get(neight, 9223372036854775807):
+			var _cost : int
+			if unit:
+				_cost = get_tile_at(neight).get_cost(unit)
+				# penalización altura
+				_cost += get_tile_at(_pos).get_height_penalty(get_tile_at(neight))
+			else:
+				_cost = 1
+			if distances.get(_pos) + _cost < distances.get(neight, MAX_INT):
 				distances.set(neight, distances.get(_pos) + _cost)
 				queue.insert({neight: distances.get(_pos) + _cost})
 				
