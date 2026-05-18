ff8247f8e9792cb37bd505875ad20da9df710da0
Daniel GV <danielgv.space@gmail.com>
2026-05-18 23:36:54 +0200

    fixbug ping

--- godot/scripts/online/packets/ping.gd
+++ godot/scripts/online/packets/ping.gd
@@ -23,9 +23,9 @@ static func create_from_data(data: PackedByteArray) -> PingPacket:
 func encode() -> PackedByteArray:
 	var data: PackedByteArray = super.encode()
 	var text := message.to_utf8_buffer()
-	data.resize(2+text.size())
+	data.resize(2)
 	data.encode_u8(1, sender_id)
-	data.append_array(message.to_utf8_buffer())
+	data.append_array(text)
 
 	return data
 
